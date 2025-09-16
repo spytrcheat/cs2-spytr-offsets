036fb4b301361839e0dd1505abf36e01e30f2986
spytrCH <spytrsever@gmail.com>
2025-09-16 20:33:38 -0300

    Add files via upload

--- scenesystem_dll.cs
+++ scenesystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-09-11 10:21:56.856903800 UTC
+// 2025-09-16 23:32:38.442205100 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: scenesystem.dll

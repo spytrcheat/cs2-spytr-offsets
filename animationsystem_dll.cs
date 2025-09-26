284ca40a6d3b20a48e961a9177ba9f738361cec8
spytrCH <spytrsever@gmail.com>
2025-09-26 07:41:53 -0300

    Add files via upload

--- animationsystem_dll.cs
+++ animationsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-09-26 00:32:02.402199700 UTC
+// 2025-09-26 10:34:24.398920600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: animationsystem.dll

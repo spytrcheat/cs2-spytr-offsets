61fa983733e0db537711fd5a3e9ab8fb9407f128
spytrCH <spytrsever@gmail.com>
2025-11-12 20:24:05 -0300

    Add files via upload

--- soundsystem_dll.cs
+++ soundsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-29 23:58:40.483993700 UTC
+// 2025-11-12 23:23:03.449781600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: soundsystem.dll

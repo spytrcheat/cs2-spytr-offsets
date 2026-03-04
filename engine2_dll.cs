77e5a8383fcd31f674cf256676a01d938508dcab
spytrCH <spytrsever@gmail.com>
2026-03-04 20:23:42 -0300

    Add files via upload

--- engine2_dll.cs
+++ engine2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-25 22:57:59.739188600 UTC
+// 2026-03-04 23:22:00.942727700 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: engine2.dll
--- host_dll.rs
+++ host_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-25 22:57:59.739188600 UTC
+// 2026-03-04 23:22:00.942727700 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 

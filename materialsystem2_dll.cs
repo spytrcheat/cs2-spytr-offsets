f55171c233d68366602d5acf4d2e377b0cac034d
spytrCH <spytrsever@gmail.com>
2026-03-09 19:51:54 -0300

    Add files via upload

--- materialsystem2_dll.cs
+++ materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-05 22:50:06.435954600 UTC
+// 2026-03-09 22:50:22.180477800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
--- materialsystem2_dll.rs
+++ materialsystem2_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-05 22:50:06.435954600 UTC
+// 2026-03-09 22:50:22.180477800 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 

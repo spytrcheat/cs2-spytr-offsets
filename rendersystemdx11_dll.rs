6ef330e62f908767499ac240376d41a4400cd504
spytrCH <spytrsever@gmail.com>
2026-03-31 17:05:46 -0300

    Add files via upload

--- rendersystemdx11_dll.rs
+++ rendersystemdx11_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-25 18:00:58.160220500 UTC
+// 2026-03-31 20:04:28.981388500 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 

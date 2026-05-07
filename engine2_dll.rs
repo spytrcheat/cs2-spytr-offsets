a34d9a86bf02c806a2294dedf4b0161ebde8fe45
spytrCH <spytrsever@gmail.com>
2026-05-07 20:30:52 -0300

    Add files via upload

--- engine2_dll.rs
+++ engine2_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-01 00:16:14.816438100 UTC
+// 2026-05-07 23:27:43.893690900 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- steamaudio_dll.zig
+++ steamaudio_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-01 00:16:14.816438100 UTC
+// 2026-05-07 23:27:43.893690900 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {

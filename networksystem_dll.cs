a44272be47922f5af27de4c55379754408c513c8
spytrCH <spytrsever@gmail.com>
2026-07-09 20:36:16 -0300

    Add files via upload

--- networksystem_dll.cs
+++ networksystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-09 10:37:39.451776400 UTC
+// 2026-07-09 23:33:49.820823400 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
